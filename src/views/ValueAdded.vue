3a59111b70a9a80afb86be03f22d2ce64c6cfdf5
Alca <karenrivasg17@gmail.com>
2023-07-24 23:36:39 -0600

    Component implementation, icons added, arts usage

--- src/views/ValueAdded.vue
+++ src/views/ValueAdded.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+import BenefitCard from "../components/BenefitCard.vue";
 </script>
 
 <template>
@@ -11,31 +11,29 @@
       </div>
     </div>
 
-    <div class="relative mt-16 space-y-20 ">
-      <img alt="" class="w-full " src="/images/coffeeBeans.jpg">
-      <div class="absolute top-1 space-y-8 max-w-screen-2xl">
+    <div class="mt-16 py-14 space-y-8 bg-coffeeBeans bg-cover bg-center">
+      <BenefitCard positionClasses="col-start-3 col-span-7"
+                   text="Café 100% cosechado en fincas salvadoreñas."/>
 
-        <div class="grid grid-cols-12 text-justify">
-          <p class="col-start-4 col-span-7 backdrop-blur-md bg-black/30 p-6 text-white
-          rounded-xl drop-shadow-2xl">
-            Café 100% cosechado en fincas salvadoreñas.
-          </p>
+      <BenefitCard positionClasses="col-start-4 col-span-7"
+                   text="Auténticos sabores distribuidos de la manera más práctica."/>
+
+      <BenefitCard positionClasses="col-start-3 col-span-7"
+                   text="Altos estándares de cálidad en los procesos de producción."/>
+
+      <div class="max-w-screen-2xl mx-auto grid grid-cols-12">
+        <div class="col-span-10 col-start-2 mt-8
+            backdrop-blur-lg bg-white/60 py-6 rounded-xl drop-shadow-2xl shadow-2xl">
+          <video autoplay class="mx-auto w-full md:w-3/5" controls="controls" height="auto"
+                 loop muted >
+            <source src="/src/assets/Post-Mestizo.m4v" type="video/mp4"/>
+          </video>
         </div>
-
-        <div class="grid grid-cols-12 text-justify">
-          <p class="col-start-5 col-span-7 backdrop-blur-lg bg-white/25 p-6 rounded-xl">
-            Auténticos sabores distribuidos de la manera más práctica.
-          </p>
-        </div>
-
-        <div class="grid grid-cols-12 text-justify">
-          <p class="col-start-4 col-span-7 backdrop-blur-md bg-white/40 p-6 rounded-xl drop-shadow-2xl">
-            Altos estándares de cálidad en los procesos de producción.
-          </p>
-        </div>
-
+        <div class="col-span-10 col-start-2"></div>
       </div>
     </div>
+
+
   </div>
 </template>
 
