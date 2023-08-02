23441b27f0ac3c6d254de7c8d15005b995e5a80c
Alca <karenrivasg17@gmail.com>
2023-08-02 11:56:51 -0600

    Content positioning

--- src/components/slider/Card.vue
+++ src/components/slider/Card.vue
@@ -7,12 +7,13 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="grid justify-items-center content-end relative h-[31rem] w-full">
+  <div class="grid mx-auto justify-items-center content-end relative h-[31rem] w-fit">
     <div class="absolute z-10">
       <img :src="'images/' + image" alt="Beverage" class="w-auto h-[22rem]">
     </div>
 
-    <div class="grid content-end rounded-xl backdrop-blur-xl bg-white/30 h-80 w-72 min-w-72 p-6 text-center">
+    <div class="grid content-end rounded-xl backdrop-blur-xl bg-white/30
+    h-80 w-72 min-w-72 py-6 px-2 text-center">
       <h1 class="text-xl font-poppins pb-3 uppercase font-extrabold text-black/80">{{ title }}</h1>
       <p class="font-montserrat font-light text-sm ">{{ description }}</p>
     </div>
